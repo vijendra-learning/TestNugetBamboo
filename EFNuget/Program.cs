a9fe9db1ad3dbba987aedbcb176c39dccb7fce5f
vijendra-learning <vijendradotnet@yahoo.com>
2018-03-23 15:20:18 +0530

    Initial setup of EFNuget

---
+++ EFNuget/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFNuget
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public int AddNumbers(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+        [MaxLength(3)]
+        public int Age { get; set; }
+    }
+}
---
+++ EFNugetTests/ProgramTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EFNuget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFNuget.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void AddNumbersTest()
+        {
+            Program myPrg = new Program();
+            var result = myPrg.AddNumbers(10, 20);
+            Assert.AreEqual(result, 30);
+        }
+    }
+}
